bc4caabf2b8a411344c978ca62b57c3ed73133b3
Maximo <maximovs@gmail.com>
2014-10-14 14:20:50 -0300

    task F: extras

--- depot/app/controllers/line_items_controller.rb
+++ depot/app/controllers/line_items_controller.rb
@@ -1,6 +1,6 @@
 class LineItemsController < ApplicationController
   include CurrentCart
-  before_action :set_cart, only: [:create]
+  before_action :set_cart, only: [:create, :destroy]
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
   # GET /line_items
@@ -59,9 +59,16 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1.json
   def destroy
     cart = @line_item.cart
-    @line_item.destroy
+    qty = @line_item.quantity - 1
+    if qty > 0
+      @line_item.quantity = qty
+      @line_item.save
+    else
+      @line_item.destroy
+    end
     respond_to do |format|
       format.html { redirect_to cart, notice: 'The item was removed from cart.' }
+      format.js { @current_item = @line_item }
       format.json { head :no_content }
     end
   end
